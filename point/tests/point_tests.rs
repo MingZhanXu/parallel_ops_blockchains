dfb0722a27464e9046f734fea5e25bdfa5e267a1
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2024-04-20 06:16:03 +0800

    增加Point的測試

--- point/tests/point_tests.rs
+++ point/tests/point_tests.rs
@@ -16,6 +16,14 @@ mod tests {
         assert_eq!(ans, add_point);
     }
     #[test]
+    fn add_point_ref() {
+        let point1 = Point::new(5.0, 6.0);
+        let point2 = Point::new(8.0, 3.0);
+        let add_point = point1 + &point2;
+        let ans = Point::new(5.0 + 8.0, 6.0 + 3.0);
+        assert_eq!(ans, add_point);
+    }
+    #[test]
     fn dis_point() {
         let point1 = Point::new(0.0, 0.0);
         let point2 = Point::new(3.0, 4.0);
@@ -25,5 +33,32 @@ mod tests {
         let ans = 5.0;
         assert_eq!(ans, dis_point_1);
         assert_eq!(ans, dis_point_2);
+    }
+    #[test]
+    fn average_point_none() {
+        let points: Vec<Point> = vec![];
+        let average_none = Point::average(&points);
+        let ans = None;
+        assert_eq!(ans, average_none);
+    }
+    #[test]
+    fn average_point() {
+        let point1 = Point::new(1.0, -1.0);
+        let point2 = Point::new(3.3, -6.0);
+        let point3 = Point::new(5.0, -9.2);
+        let points: Vec<Point> = vec![point1, point2, point3];
+        let average_none = Point::average(&points);
+        let total_x = 1.0 + 3.3 + 5.0;
+        let total_y = -1.0 + -6.0 + -9.2;
+        let ans = Some(Point::new(total_x / 3.0, total_y / 3.0));
+        assert_eq!(ans, average_none);
+    }
+    #[test]
+    fn eq_point() {
+        let point1 = Point::new(1.0, -1.0);
+        let point2 = Point::new(1.0, -1.0);
+        let point_eq = point1 == point2;
+        let ans = true;
+        assert_eq!(ans, point_eq);
     }
 }
