36def3b90397375071030115038dcb5f3af940c6
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2024-04-20 07:17:43 +0800

    Update lib.rs

--- keams/src/lib.rs
+++ keams/src/lib.rs
@@ -1,1 +1,25 @@
-use point;
+use std::collections::HashSet;
+
+use rand::Rng;
+use point::*;
+pub fn rand_point(start: f64, end: f64) -> Point {
+    let x = rand::thread_rng().gen_range(start..end as f64);
+    let y = rand::thread_rng().gen_range(start..end as f64);
+    Point::new(x, y)
+}
+pub fn rand_points(start: f64, end: f64, num: usize) -> Vec<Point> {
+    let mut points: HashSet<Point> = HashSet::new();
+    while points.len() < num {
+        let p = rand_point(start, end);
+        points.insert(p);
+    }
+    points.into_iter().collect()
+}
+pub fn rand_center(max: usize, num: usize) -> Vec<usize> {
+    let mut center: HashSet<usize> = HashSet::new();
+    while center.len() < num {
+        let n = rand::thread_rng().gen_range(0..max as usize);
+        center.insert(n);
+    }
+    center.into_iter().collect()
+}
