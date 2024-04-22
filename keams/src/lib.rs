a258411d73b630978d6321f1bec0610a309a8746
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2024-04-22 16:54:06 +0800

    完成分群與部分測試

--- keams/src/lib.rs
+++ keams/src/lib.rs
@@ -1,7 +1,8 @@
-use std::collections::HashSet;
+use std::{collections::HashSet};
 
 use rand::Rng;
 use point::*;
+
 pub fn rand_point(start: f64, end: f64) -> Point {
     let x = rand::thread_rng().gen_range(start..end as f64);
     let y = rand::thread_rng().gen_range(start..end as f64);
@@ -15,11 +16,49 @@ pub fn rand_points(start: f64, end: f64, num: usize) -> Vec<Point> {
     }
     points.into_iter().collect()
 }
-pub fn rand_center(max: usize, num: usize) -> Vec<usize> {
-    let mut center: HashSet<usize> = HashSet::new();
-    while center.len() < num {
-        let n = rand::thread_rng().gen_range(0..max as usize);
-        center.insert(n);
+
+pub fn rand_centers(centers_len: usize, points_len: usize) -> Vec<usize> {
+    let mut centers: HashSet<usize> = HashSet::new();
+    while centers.len() < centers_len {
+        let n = rand::thread_rng().gen_range(0..points_len as usize);
+        centers.insert(n);
     }
-    center.into_iter().collect()
+    centers.into_iter().collect()
+}
+
+pub fn cluster_range(len: usize, user_id: usize, user_max: usize) -> (usize, usize) {
+    let range = len / user_max;
+    let start = range * user_id;
+    let end;
+    if user_id == user_max - 1 {
+        end = len;
+    } else if user_id < user_max {
+        end = start + range - 1;
+    } else {
+        panic!("設定範圍錯誤");
+    }
+    (start, end)
+}
+fn min_dis(point: &Point, points: &Vec<Point>, centers: &Vec<usize>) -> usize {
+    let mut min_dis = std::f64::MAX;
+    let mut index = 0;
+    for (i, c) in centers.iter().enumerate() {
+        let dis = point.dis(&points[*c]);
+        if min_dis > dis {
+            min_dis = dis;
+            index = i;
+        }
+    }
+    index
+}
+pub fn cluster(points: &Vec<Point>, centers: &Vec<usize>, user_id: usize, user_max: usize) -> Vec<Vec<usize>> {
+    let points_len = points.len();
+    let centers_len = centers.len();
+    let (start, end) = cluster_range(points_len, user_id, user_max);
+    let mut teams: Vec<Vec<usize>> = vec![vec![]; centers_len];
+    for (i, p) in points[start..end].iter().enumerate() {
+        let team_index = min_dis(p, points, centers);
+        teams[team_index].push(i + start);
+    }
+    teams
 }
---
+++ keams/tests/keams_test.rs
@@ -0,0 +1,68 @@
+#[cfg(test)]
+mod tests {
+    use std::result;
+
+    use keams::*;
+    #[test]
+    fn rand_one_point() {
+        let start = 0.0;
+        let end = 1024.0;
+        let point = rand_point(start, end);
+        assert!(point.x() >= start && point.x() <= end);
+        assert!(point.y() >= start && point.y() <= end);
+    }
+    #[test]
+    fn rand_points_different() {
+        let start = 0.0;
+        let end = 1024.0;
+        let num = 10;
+        let point = rand_points(start, end, num);
+        for (i, p1) in point.iter().enumerate() {
+            for (j, p2) in point.iter().enumerate() {
+                if i != j {
+                    assert_ne!(p1, p2);
+                }
+            }
+        }
+    }
+    #[test]
+    fn rand_center_different() {
+        let centers_len = 4;
+        let points_len = 50;
+        let centers = rand_centers(centers_len, points_len);
+        for (i, c1) in centers.iter().enumerate() {
+            for (j, c2) in centers.iter().enumerate() {
+                if i != j {
+                    assert_ne!(c1, c2);
+                }
+            }
+        }
+    }
+    #[test]
+    fn cluster_range_same_id_zero() {
+        let len = 10;
+        let user_id = 0;
+        let user_max = 3;
+        let range = cluster_range(len, user_id, user_max);
+        let ans = (0, 2);
+        assert_eq!(ans, range);
+    }
+    #[test]
+    fn cluster_range_same_id_max() {
+        let len = 10;
+        let user_id = 2;
+        let user_max = 3;
+        let range = cluster_range(len, user_id, user_max);
+        let ans = (6, 10);
+        assert_eq!(ans, range);
+    }
+    // #[test]
+    // fn cluster_range_panic() {
+    //     let len = 10;
+    //     let user_id = 3;
+    //     let user_max = 3;
+    //     let result = cluster_range(len, user_id, user_max);
+    //     let ans = (6, 10);
+    //     assert_eq!(ans, range);
+    // }
+}
